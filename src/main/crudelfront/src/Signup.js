589bf57246903466bfc92cd7fd437ff53fd7cd55
anupreehimm <anupreethimuruganmec@gmail.com>
2022-09-06 13:05:27 +0530

    final front

--- src/main/crudelfront/src/Signup.js
+++ src/main/crudelfront/src/Signup.js
@@ -39,45 +39,45 @@ export const SignUp=()=>{
     }
 
     return(
-        <>
+            <>
             <div className="container mt-5">
                 <div className="row justify-content-center">
                     <div className="col-lg-7 col-md-8 col-sm-12 shadow rounded-5 p-5">
                         <div className="form group">
                             <label>Full Name</label>
-                            <input type="text" name="employee" placeholder="Enter employee name" className="form-contorl" value={sign.empName} onChange={gather}></input>
+                            <input type="text" name="empName" placeholder="enter the name" className="form-control" value={sign.empName
+                            } onChange={gather} />
                         </div>
-                        <div className="form group">
-                            <label>User Name</label>
-                            <input type="text" name="username" placeholder="Enter user name" className="form-contorl" value={sign.username} onChange={gather}></input>
-                        </div>
-                        <div className="form group">
-                            <label>password please</label>
-                            <input type="password" name="password" placeholder="Enter your password" className="form-contorl" value={sign.password} onChange={gather}></input>
-                        </div>
-                        <div className="form group">
-                            <label>Employee mobileNo</label>
-                            <input type="text" name="mobileNo" placeholder="Enter employee mobileno" className="form-contorl" value={sign.mobileNo} onChange={gather}></input>
-                        </div>  
-                        <div className="form group">
-                            <label>Employee emailid</label>
-                            <input type="text" name="email" placeholder="Enter employee email" className="form-contorl" value={sign.email} onChange={gather}></input>
-                        </div>
-                        <div className="mt-4 row justify-content-around">
-                            <button className="col-1 btn btn-success" onClick={onSign}>
-                                <i class="bi bi-person-circle"></i>
-                            </button>
-                            <button className="col-1 btn btn-dark" onClick={OnRest}>
-                                <i class="bi bi-slash-circle"></i>
-                            </button>
-                        </div>
-
-
-                    </div>
-
+                <div className="form group">
+                    <label>User Name</label>
+                    <input type="text" name="username" placeholder="enter the username" className="form-control" value={sign.username} onChange={gather} />
                 </div>
-
+                <div className="form group">
+                    <label>Set password</label>
+                    <input type="password" name="password" placeholder="enter the password" className="form-control" value={sign.password} onChange={gather} />
+                </div>
+                <div className="row justify-content-between">
+                    <div className="col-lg-6 col-sm-12">
+                        <label>Contact number</label>
+                        <input type="text" name="mobileNo" placeholder="enter the mobile" className="form-control" value={sign.mobileNo} onChange={gather} />
+                    </div>
+                    <div className="col-lg-6 col-sm-12">
+                        <label>Email</label>
+                        <input type="text" name="email" placeholder="enter the email" className="form-control" value={sign.email} onChange={gather} />
+                    </div>
+                </div>
+                <div className="mt-4 row justify-content-around">
+                    <button className="col-1 btn btn-success" onClick={onSign}>
+                        <i class="bi bi-person-circle"></i>
+                    </button>
+                    <button className="col-1 btn btn-dark" onClick={OnRest}>
+                        <i class="bi bi-slash-circle"></i>
+                    </button>
+                </div>
             </div>
-        </>
+        </div>
+    </div>
+</>
+            
     )
 }
